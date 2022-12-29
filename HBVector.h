07b36021ca27ef3bd4e5e3029f39e41f55aae53a
Hend Ahmed <92755637+LifelongLearner-HEND@users.noreply.github.com>
2022-12-29 23:56:19 +0200

    Add files via upload

--- HBVector.h
+++ HBVector.h
@@ -1,85 +1,90 @@
-#ifndef HEADER_H
-#define HEADER_H
+// FCAI – Object-Oriented Programming 1 – 2022 - Assignment 3
+// Program Name: HBVector
+// Last Modification Date: 23/12/2022
+// Author1 and ID and Group: Hend Ahmed Kamal Abd-El Aziem  ID: 20211111
+// Author2 and ID and Group: Basma Mahmoud Hashim Ali  ID: 20210091
+/*
+description: In this problem we developed our own C++ vector template class.
+that can hold some functions .
+such as: begin ,end , erase, pop_back, push_back, etc...
+ */
 
+#ifndef HBVECTOR_H
+#define HBVECTOR_H
+#define TMP template <class T>
+#include <iterator>
 #include <iostream>
 #include <string>
-#include <iterator>
 using namespace std;
-
-#define TMP template <class T>
-
+// Iterator class with implementation
 TMP
-class Iterator
+class Iterator {
+private:
+    T* ptr;
+
+public:
+    Iterator(T* ptr) : ptr(ptr) {}
+    Iterator& operator++() { ptr++; return *this; }
+    Iterator operator++(int) { Iterator tmp(*this); operator++(); return tmp; }
+    Iterator& operator--() { ptr--; return *this; }
+    Iterator operator--(int) { Iterator tmp(*this); operator--(); return tmp; }
+    bool operator==(const Iterator& rhs) { return ptr == rhs.ptr; }
+    bool operator!= (const Iterator& rhs) { return ptr != rhs.ptr; }
+    bool operator< (const Iterator& rhs) { return ptr < rhs.ptr; }
+    bool operator> (const Iterator& rhs) { return ptr > rhs.ptr; }
+    bool operator>= (const Iterator& rhs) { return ptr >= rhs.ptr; }
+    bool operator<= (const Iterator& rhs) { return ptr <= rhs.ptr; }
+    bool operator- (const Iterator& rhs) { return ptr - rhs.ptr; }
+    bool operator+ (const Iterator& rhs) { return ptr + rhs.ptr; }
+    Iterator operator + (int n) { Iterator tmp(ptr + n); return tmp; }
+    Iterator operator - (int n) { Iterator tmp(ptr - n); return tmp; }
+    T* operator -> () { return ptr; }
+    T& operator* () { return *ptr; }
+};
+
+//--------------------------------------------------------------------------------------------
+// HBVector class
+template <typename T>
+class HBVector
 {
 private:
-	T* ptr;
-	
+    T* data;
+    int Size, Capacity;
+
 public:
-	Iterator(T* ptr) : ptr(ptr) {}
-	Iterator(Iterator<T> const& itr) : ptr(itr.ptr) {}
-	Iterator& operator++() { ptr++; return *this; }
-	Iterator operator++(int) { Iterator tmp(*this); operator++(); return tmp; }
-	bool operator==(const Iterator& rhs) { return ptr == rhs.ptr; }
-	bool operator!=(const Iterator& rhs) { return ptr != rhs.ptr; }
-	T& operator*() { return *ptr; }
+    HBVector(int cap = 10);
+    HBVector(T* arr ,int n);
+    HBVector(HBVector<T>& vec);
+    T& operator[] (int index);
+    HBVector<T> &operator= (HBVector<T> &another);
+    HBVector<T> &operator= (HBVector<T> &&another);
+    ~HBVector();
+
+    void push_back(T element);
+    T pop_back();
+    void erase(Iterator<T> iter);
+    Iterator <T> begin();
+    Iterator <T> end();
+    void erase(Iterator<T> iter1, Iterator<T> iter2);
+    void clear();
+    void insert(Iterator<T>, T element);
+
+    int size() const;
+    int capacity() const;
+    void resize(int newCap);
+    bool empty();
+    template <typename F>
+    friend ostream& operator<< (ostream& out, HBVector<F>& vec);
+
 };
 
-TMP
-class HBVector {
-private:
-	T* data;
-	int Size, Capacity;
-public:
-	// Constructors and Big 4 
-	HBVector(int cap);								// Initialize by specific capacity
-		// No content is added, size = 0
-		// Assign a default size value
-	HBVector(T* arr, int  n);						// Initialize by n items from array 
-	HBVector(const HBVector& other);					// Initialize with a copy
-	~HBVector();									// Delete allocated memory
-	HBVector& operator=(const HBVector& other);			// Copy assignment  
-	HBVector& operator=(const HBVector&& other);		// Move assignment 
+template<typename F>
+ostream &operator<<(ostream &out, HBVector<F> &vec) {
+    for (int i = 0; i < vec.Size; ++i) {
+        out << vec[i] << " ";
+    }
+    return out;
+}
 
-	// Access operations 
-	T& operator[](int index); // Access item by reference 
+#endif // !HBVECTOR_H
 
-	// Throw an exception if out of range 
-///////////////////////////////////////////////////////////////////////////////////
-	
-// Modifying operations 
-	int push_back(T element);										// Add item to end of vec & return # of items 
-																	// Increase capacity of needed
-	T pop_back();													// Remove and return last element in vec 
-	void erase(Iterator<T> itr);									// Remove item at iterator
-																	// Throw exception if invalid iter 
-	void erase(Iterator<T> iterator1, Iterator<T> iterator2);		// Remove items between 
-																	// iterator 1 <= iterator 2 otherwise do nothing
-																	// Throw exception if any iterator outside range
-	void clear();													// Delete all vector content
-	void insert(Iterator<T>, T);									// Insert item at iterator 
-																	// Throw exception if invalid 
-
-	// Iterators 								// Supports *, + and ++ operations at least
-	// Can use: typedef T* iterator 
-												// Or u can use std::iterator so you can 
-	// apply STL algorithms on HBVector
-		Iterator<T> begin();						// Return an iterator (T*) 
-		Iterator<T> end();							// Return an iterator (T*) 
-///////////////////////////////////////////////////////////////////////////////////
-	// Comparison operations 
-		bool operator==(const HBVector<T>&);		// Return true if ==  
-		bool operator< (const HBVector<T>&);			// Compares item by item
-	// Return true if first different item in this is < in other
-
-	// Capacity operations 
-		int size() const;						// Return current size of vec
-		int capacity() const;					// Return size of current allocated array  
-		void resize(int newCap);				// Relocate to bigger space
-		bool empty();							// Return true if size is 0
-
-	// Friends 
-		friend ostream &operator<< (ostream& out, HBVector<T> vec);
-};
-
-
-#endif
